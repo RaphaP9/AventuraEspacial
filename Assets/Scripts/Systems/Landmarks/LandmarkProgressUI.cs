773f195f6cb1bef98493ee8ca490c1b25ae4fe9d
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-10-05 16:06:43 -0500

    ADD LandmarkProgressUI 3

--- Assets/Scripts/Systems/Landmarks/LandmarkProgressUI.cs
+++ Assets/Scripts/Systems/Landmarks/LandmarkProgressUI.cs
@@ -11,19 +11,14 @@ public class LandmarkProgressUI : MonoBehaviour
 
     [Header("Components")]
     [SerializeField] private Slider landmarkProgressSlider;
-    [Space]
-    [SerializeField] private List<LandmarkUI> landmarkUIList;
 
     [Header("RuntimeFilled")]
     [SerializeField] private int currentMinigameScore;
     [SerializeField] private int finalMinigameScoreLandmark;
 
-
     private void Start()
     {
         CalculateScores();
-        SetLandmarks();
-
         UpdateLandmarkSlider();
     }
 
@@ -31,37 +26,6 @@ public class LandmarkProgressUI : MonoBehaviour
     {
         currentMinigameScore = DataContainer.Instance.GetMinigameTotalScoreByMinigame(minigame);
         finalMinigameScoreLandmark = landmarkSettings.landmarkScore3;
-    }
-
-    private void SetLandmarks()
-    {
-        foreach(LandmarkUI landmarkUI in landmarkUIList)
-        {
-            SetLandmark(landmarkUI);
-        }
-    }
-
-    private void SetLandmark(LandmarkUI landmarkUI)
-    {
-        int targetScore;
-        bool landmarkUnlocked = DataContainer.Instance.GetLandmarkUnlockedByMinigame(minigame, landmarkUI.MinigameLandmark);
-        bool landmarkChecked = DataContainer.Instance.GetLandmarkCheckedByMinigame(minigame, landmarkUI.MinigameLandmark);
-
-        switch (landmarkUI.MinigameLandmark)
-        {
-            case MinigameLandmark.FirstLandmark:
-            default:
-                targetScore = landmarkSettings.landmarkScore1;
-                break;
-            case MinigameLandmark.SecondLandmark:
-                targetScore = landmarkSettings.landmarkScore2;
-                break;
-            case MinigameLandmark.ThirdLandmark:
-                targetScore = landmarkSettings.landmarkScore3;
-                break;
-        }
-
-        landmarkUI.SetLandmark(minigame, targetScore, landmarkUnlocked, landmarkChecked);
     }
 
     private void UpdateLandmarkSlider()
