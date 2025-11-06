56e2c8f888857af7639f6fa916b96f48f75321bf
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-11-06 00:42:16 -0500

    ADD Collectables 2

---
+++ Assets/Scripts/Systems/Collectables/CollectionHandlers/Concretions/Collectable3CollectionHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Collectable3CollectionHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
