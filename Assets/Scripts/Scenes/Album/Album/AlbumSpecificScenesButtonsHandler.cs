c69765a627aca8adc3ab00b72867e12418045926
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-11-18 14:34:55 -0500

    ADD Album New Scenes 2

---
+++ Assets/Scripts/Scenes/Album/Album/AlbumSpecificScenesButtonsHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class AlbumSpecificScenesButtonsHandler : MonoBehaviour
+{
+    [Header("Lists")]
+    [SerializeField] private List<AlbumSceneButtonRelationship> albumSceneButtonRelationships;
+
+    [System.Serializable]
+    public class AlbumSceneButtonRelationship
+    {
+        public Button button;
+        public string sceneName;
+        public TransitionType sceneTransitionType;
+    }
+
+    private void Awake()
+    {
+        InitializeButtonsListeners();
+    }
+
+    private void InitializeButtonsListeners()
+    {
+        foreach(AlbumSceneButtonRelationship relationship in albumSceneButtonRelationships)
+        {
+            relationship.button.onClick.AddListener(() => LoadAlbumSpecificScene(relationship.sceneName, relationship.sceneTransitionType));
+        }
+    }
+
+    private void LoadAlbumSpecificScene(string sceneName, TransitionType sceneTransitionType)
+    {
+        ScenesManager.Instance.TransitionLoadTargetScene(sceneName, sceneTransitionType);
+    }
+}
